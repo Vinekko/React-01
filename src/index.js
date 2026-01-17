e83898966416cad089c7a99aeccd961440088c0a
Vincenzo Trigila <nitomaco@gmail.com>
2026-01-17 16:59:51 -0400

    components gallery

--- src/index.js
+++ src/index.js
@@ -2,8 +2,31 @@ import React from "react";
 import ReactDom from "react-dom/client";
 
 //todos los componentes retornan UN elemento html 
-function App() {
-    return <h1>Hello, World!</h1>;
+// function App() {
+//     return <h1>Hello, World!</h1>;
+// }
+
+//parte 2
+function Profile() {
+    return (
+        <img
+            src="https://static0.cbrimages.com/wordpress/wp-content/uploads/2019/05/Joker-Persona-5-feature.jpg"
+            alt="Persona 5"
+            width={500}
+        />
+    );
+
+}
+
+export default function Gallery() {
+    return (
+        <section>
+            <h1>Personajes de Videojuegos</h1>
+            <Profile />
+            <Profile />
+            <Profile />
+        </section>
+    );
 }
 
 const root = ReactDom.createRoot(
@@ -11,5 +34,7 @@ const root = ReactDom.createRoot(
 )
 
 root.render(
-    <App/>
+    <React.StrictMode>
+        <Gallery />
+    </React.StrictMode>
 )
